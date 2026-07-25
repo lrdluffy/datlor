93d610299693d10d35c88ee41dd1cc204a62ca8f
lrdluffy <lrdaftkhar@gmail.com>
2026-07-25 03:21:05 +0000

    refactor(core-service): rename base package to com.strawhats

---
+++ backend-services/core-service/src/main/java/com/strawhats/core/CoreServiceApplication.java
@@ -0,0 +1,14 @@
+package com.strawhats.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+public class CoreServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreServiceApplication.class, args);
+    }
+}
