820df8905f3180bdd0c466d1e648072956dab388
lrdluffy <lrdaftkhar@gmail.com>
2026-08-10 03:53:45 +0000

    chore(media-service): add maven build, Dockerfile, and app config

---
+++ backend-services/media-service/src/main/java/com/strawhats/media/MediaServiceApplication.java
@@ -0,0 +1,14 @@
+package com.strawhats.media;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+public class MediaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediaServiceApplication.class, args);
+    }
+}
